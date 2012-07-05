649aa42b5385ee7a4286791a8bf1bc2ae7e122b3
Max Kirillov <max@max630.net>
2012-07-05 07:54:52 +0300

    refactor

--- WalkClass.hs
+++ WalkClass.hs
@@ -20,35 +20,35 @@ $(let
       do
         f <- newName "f"
         td0 <- reify tName
-        tDatas <- case td0 of
-          TyConI (DataD [] _ [] tcs []) ->
-                  mapM (\ (NormalC conName conTypes) ->
-                              do
-                                let
-                                  l = length conTypes
-                                  addTypes = concat $ map (\ (_, t) -> getTypes t) conTypes
-                                v0s <- sequence $ map (\n -> newName ("v0_" ++ nameBase conName ++ "_" ++ show n)) [1 .. l]
-                                v1s <- sequence $ map (\n -> newName ("v1_" ++ nameBase conName ++ "_" ++ show n)) [1 .. l]
-                                stOs <- sequence $ map (\n -> newName ("stO_" ++ nameBase conName ++ "_" ++ show n)) [1 .. l]
-                                return (conName, v0s, v1s, stOs, addTypes))
-                       tcs
+        case td0 of
+          TyConI (DataD [] _ [] tcs []) -> do
+                  tDatas <- mapM (\ (NormalC conName conTypes) ->
+                                      do
+                                        let
+                                          l = length conTypes
+                                          addTypes = concat $ map (\ (_, t) -> getTypes t) conTypes
+                                        v0s <- sequence $ map (\n -> newName ("v0_" ++ nameBase conName ++ "_" ++ show n)) [1 .. l]
+                                        v1s <- sequence $ map (\n -> newName ("v1_" ++ nameBase conName ++ "_" ++ show n)) [1 .. l]
+                                        stOs <- sequence $ map (\n -> newName ("stO_" ++ nameBase conName ++ "_" ++ show n)) [1 .. l]
+                                        return (conName, v0s, v1s, stOs, addTypes))
+                                tcs
+                  let
+                    clauseFromtData (conName, v0s, v1s, stOs, _) =
+                             -- \ ,f (,conName ,v0s[0] ...) -> do
+                             --     (,v1s[0], ,stOs[0]) <- walk ,f ,v0s[0]
+                             --     ...
+                             --     return (,conName ,v1s[0] ,v1s[1] ..., mconcat [,stOs[0], ,stOs[1] ...])
+                             Clause [VarP f, ConP conName (map VarP v0s)]
+                                (NormalB (DoE (
+                                          zipWith3 (\v0 v1 stO ->
+                                                      BindS (TupP [VarP v1, VarP stO])
+                                                            (foldl AppE (VarE $ mkName "walk") [VarE f, VarE v0]))
+                                                   v0s v1s stOs
+                                          ++ [NoBindS (AppE (VarE $ mkName "return")
+                                                            (TupE [foldl AppE (ConE conName) (map VarE v1s), AppE (VarE $ mkName "mconcat") (ListE $ map VarE stOs)]))]
+                                        ))) []
+                  return (FunD walkName (map clauseFromtData tDatas), uniq $ concat $ map (\ (_, _, _, _, ts) -> ts) tDatas)
           _ -> fail ("not a simple data declaration: " ++ show td0)
-        let
-          clauseFromtData (conName, v0s, v1s, stOs, _) =
-                   -- \ ,f (,conName ,v0s[0] ...) -> do
-                   --     (,v1s[0], ,stOs[0]) <- walk ,f ,v0s[0]
-                   --     ...
-                   --     return (,conName ,v1s[0] ,v1s[1] ..., mconcat [,stOs[0], ,stOs[1] ...])
-                   Clause [VarP f, ConP conName (map VarP v0s)]
-                      (NormalB (DoE (
-                                zipWith3 (\v0 v1 stO ->
-                                            BindS (TupP [VarP v1, VarP stO])
-                                                  (foldl AppE (VarE $ mkName "walk") [VarE f, VarE v0]))
-                                         v0s v1s stOs
-                                ++ [NoBindS (AppE (VarE $ mkName "return")
-                                                  (TupE [foldl AppE (ConE conName) (map VarE v1s), AppE (VarE $ mkName "mconcat") (ListE $ map VarE stOs)]))]
-                              ))) []
-        return (FunD walkName (map clauseFromtData tDatas), uniq $ concat $ map (\ (_, _, _, _, ts) -> ts) tDatas)
     getTypes (AppT t1 t2) = (getTypes t1) ++ (getTypes t2)
     getTypes (ConT n) | elem (nameBase n) ["Maybe", "[]", "(,)"] = []
     getTypes (ConT n) = [nameBase n]
