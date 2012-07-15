035e3ce248d6505329bb682995848a1fbd3af0e7
Max Kirillov <max@max630.net>
2012-07-15 09:37:28 +0300

    BEGIN: start removing parameter handling from Walkable - lets' monad handle this

--- Walkable/Class.hs
+++ Walkable/Class.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 module Walkable.Class where
 
-import Control.Monad (mapAndUnzipM)
+import Control.Monad (liftM)
 import Data.Monoid (Monoid, mempty, mappend, mconcat)
 import Language.Haskell.TH.Syntax (Quasi)
 
@@ -9,26 +9,18 @@ import Language.Haskell.TH.Syntax (Quasi)
 -- * generic parameter type (this probably requires another class)
 -- * input state?
 class (Quasi m) => Walkable m a b where
-  walk :: Monoid stO => (b -> m (b, stO)) -> a -> m (a, stO)
-
-
+  walk :: (b -> m b) -> a -> m a
 
 instance (Quasi m, Walkable m a1 b, Walkable m a2 b) => Walkable m (a1, a2) b where
   walk f (e1, e2) =
     do
-      (e1', stO1) <- walk f e1
-      (e2', stO2) <- walk f e2
-      return ((e1', e2'), mappend stO1 stO2)
+      e1' <- walk f e1
+      e2' <- walk f e2
+      return (e1', e2')
 
 instance (Quasi m, Walkable m a b) => Walkable m [a] b where
-  walk f es =
-    do
-      (es', stOs) <- mapAndUnzipM (walk f) es
-      return (es', mconcat stOs)
+  walk f es = mapM (walk f) es
 
 instance (Quasi m, Walkable m a b) => Walkable m (Maybe a) b where
-  walk f Nothing = return (Nothing, mempty)
-  walk f (Just v) =
-    do
-      (e', stO) <- walk f v
-      return (Just e', stO)
+  walk f Nothing = return Nothing
+  walk f (Just v) = liftM Just (walk f v)
