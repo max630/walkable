c5c6ce0a88fa6714efdc471706b47ff61789a65d
Max Kirillov <max@max630.net>
2012-07-16 23:11:08 +0300

    IMPL: Simplified Walkable class

--- Walkable/Class.hs
+++ Walkable/Class.hs
@@ -2,26 +2,25 @@
 module Walkable.Class where
 
 import Control.Monad (liftM)
-import Language.Haskell.TH.Syntax (Quasi)
 
 -- TODO:
 -- * generic parameter type (this probably requires another class)
-class (Quasi m) => Walkable m a b where
+class (Monad m) => Walkable m a b where
   walk :: (b -> m b) -> a -> m a
 
 -- Walkable uses m to handle context. For example, Writer can keep scope variables.
 -- Also input can also be
 
-instance (Quasi m, Walkable m a1 b, Walkable m a2 b) => Walkable m (a1, a2) b where
+instance (Monad m, Walkable m a1 b, Walkable m a2 b) => Walkable m (a1, a2) b where
   walk f (e1, e2) =
     do
       e1' <- walk f e1
       e2' <- walk f e2
       return (e1', e2')
 
-instance (Quasi m, Walkable m a b) => Walkable m [a] b where
+instance (Monad m, Walkable m a b) => Walkable m [a] b where
   walk f es = mapM (walk f) es
 
-instance (Quasi m, Walkable m a b) => Walkable m (Maybe a) b where
+instance (Monad m, Walkable m a b) => Walkable m (Maybe a) b where
   walk f Nothing = return Nothing
   walk f (Just v) = liftM Just (walk f v)
