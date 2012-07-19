15bdfa45dd1e3fb083a9f650f2cc1cdd6f780a00
Max Kirillov <max@max630.net>
2012-07-20 01:57:19 +0300

    DOC: fix comment: moved all to a single place

--- Walkable/Template.hs
+++ Walkable/Template.hs
@@ -29,10 +29,29 @@ import qualified Data.Set as S
 -- what is wanted from template is the "combination" and "tName". All the rest can be submitted by client
 -- (monad context is also mandatory for now)
 -- also, what parameters are added, there can be portion added to the context
+
+-- recurses through types and generates:
+-- ,startName :: {- makeInstances::startFuncType -} Monad m (,paramType -> m ,paramType) -> ,startType -> m ,startType
+-- ,startName = .. same as makeSingleWalk
+--
+-- {- makeSingleInstance -}
+-- instance Walkable ,tName ,paramType where
+--  {- makeSingleWalk -} 
+--  walk ,f ,d = ((\,wC -> case ,d of
+--                  {- makeSingleWalk::clauseFromtData -}
+--                  (,conName ,v0s[0] ...) -> do
+--                    ,v1s[0], <- ,wC ,v0s[0]
+--                    ...
+--                    return (,conName ,v1s[0] ,v1s[1] ...)
+--                    ...
+--                  ) :: {- makeSingleWalk::cType -} Monad m (forall v . Walkable v => v -> m v) -> m ,tName) (walk ,f)
+--
+-- {- makeEmpty -}
+-- instance Walkable ,tName ,paramType where
+--  walk ,f ,e = return ,e
+--
 makeInstances paramType startType startName empty real ignore = do
   -- TODO: error reporting
-  -- ,startName :: startFuncType
-  -- ,startName = ...
   startFuncType <- [t|Monad m => ($(return paramType) -> m $(return paramType)) -> $(conT startType) -> m $(conT startType)|]
   (Just startRes, startDeps) <- runWriterT (makeSingleWalk startName startType paramType)
   cycle S.empty [SigD startName startFuncType, startRes] paramType (S.filter (not . ignore) startDeps)
@@ -72,10 +91,6 @@ makeSingleWalk walkName tName paramType =
               wC <- lift $ newName "wC" -- walkClosure name
               d <- lift $ newName "d" -- data
               let
-                -- (,conName ,v0s[0] ...) -> do
-                --   ,v1s[0], <- ,wC ,v0s[0]
-                --   ...
-                --   return (,conName ,v1s[0] ,v1s[1] ...)
                 clauseFromtData (conName, v0s, v1s) =
                   Match (ConP conName (map VarP v0s))
                      (NormalB (DoE (
@@ -88,10 +103,6 @@ makeSingleWalk walkName tName paramType =
                              ))) []
               cType <- lift [t|Monad m => (forall v . Walkable v $(return paramType) => v -> m v) -> m $(conT tName)|]
               walkClosure <- lift [|walk $(varE f)  |]
-              -- \ ,f ,d ->
-              --   ((\ ,wC -> case ,d of
-              --      ...
-              --    ) :: ,cType) (walk ,f)
               return $ Just (FunD walkName
                               [
                                 Clause [VarP f, VarP d]
@@ -111,12 +122,9 @@ makeSingleWalk walkName tName paramType =
     tellTypes ListT = return ()
     tellTypes (TupleT _) = return ()
 
--- TODO: make the same as for real
 makeSingleInstance tName paramType =
   do
     (decWalk, dependencies) <- runWriterT $ makeSingleWalk 'walk tName paramType
-    -- instance Walkable ,tName ,paramType where
-    --  ,decWalk
     return (fmap  (\d -> InstanceD
                             []
                             (foldl AppT (ConT ''Walkable) [ConT tName, paramType])
@@ -124,12 +132,11 @@ makeSingleInstance tName paramType =
                   decWalk
            , dependencies)
 
+-- TODO: make the same as for real
 makeEmpty tName paramType =
   do
     f <- newName "f"
     e <- newName "e"
-    -- instance Walkable ,tName ,paramType where
-    --  walk ,f ,e = return ,e
     return $ InstanceD []
                        (foldl AppT (ConT ''Walkable) [ConT tName, paramType])
                        [FunD 'walk
