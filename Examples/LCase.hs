9368b28d517a453b323a53c84fa89491df4f5454
Max Kirillov <max@max630.net>
2012-11-01 23:20:31 +0200

    used Data.Data instead of this library

--- Examples/LCase.hs
+++ Examples/LCase.hs
@@ -1,29 +1,35 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, LambdaCase #-}
 module Examples.LCase where
 
 import Language.Haskell.TH
 import Control.Monad(liftM)
 
-import Control.Walkable.Class (walk)
-import Control.Walkable.THExp (walkExpImpl, ExpHandler(ExpHandler))
+import Data.Data(Data, gmapM)
+import Data.Generics.Aliases(extM)
 
 lcase :: [a -> b] -> a -> b
 lcase = undefined
 
-lcaseHandler = ExpHandler handle
-  where
-    handle (AppE lcaseV clauses) | lcaseV == (VarE 'lcase) =
-      do
-        var <- newName "var"
-        clauses' <- case clauses of
-                      ListE cl -> return cl
-                      _ -> fail ("handleLCase: clauses must me list of lambdas: " ++ show (ppr clauses))
-        clauses'' <-
-          mapM (\v -> case v of
-                        LamE [p] e -> liftM (\e' -> Match p (NormalB e') []) $ walk lcaseHandler e
-                        _ -> fail ("handleLCase: only single-pattern lambda is allowed in clauses: " ++ show (ppr v)))
-                clauses'
-        return (LamE [VarP var] (CaseE (VarE var) clauses''))
-    handle exp = walkExpImpl lcaseHandler exp
+handleLCase expQ = expQ >>= descentLCase
 
-handleLCase expQ = expQ >>= walk lcaseHandler
+descentLCase :: Data d => d -> Q d
+descentLCase = gmapM handler
+  where
+    handler :: Data d => d -> Q d
+    handler = recurse `extM` (\case
+                AppE lcaseV clauses | lcaseV == (VarE 'lcase) ->
+                  do
+                    var <- newName "var"
+                    clauses' <- case clauses of
+                                  ListE cl -> return cl
+                                  _ -> fail ("handleLCase: clauses must me list of lambdas: " ++ show (ppr clauses))
+                    clauses'' <-
+                      mapM (\v -> case v of
+                                    LamE [p] e -> liftM (\e' -> Match p (NormalB e') []) $ descentLCase e
+                                    _ -> fail ("handleLCase: only single-pattern lambda is allowed in clauses: " ++ show (ppr v)))
+                            clauses'
+                    return (LamE [VarP var] (CaseE (VarE var) clauses''))
+                e -> recurse e
+              )
+    -- recurse :: Data d => d -> Q d
+    recurse v = gmapM descentLCase v
