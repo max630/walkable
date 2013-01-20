438711bc82f3eafcf599a94fb4acce356bf86f77
Max Kirillov <max@max630.net>
2013-01-20 21:24:31 +0200

    Revert "used Data.Data instead of this library"
    This reverts commit 9368b28d517a453b323a53c84fa89491df4f5454.

--- Examples/LCase.hs
+++ Examples/LCase.hs
@@ -1,35 +1,29 @@
-{-# LANGUAGE TemplateHaskell, LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Examples.LCase where
 
 import Language.Haskell.TH
 import Control.Monad(liftM)
 
-import Data.Data(Data, gmapM)
-import Data.Generics.Aliases(extM)
+import Control.Walkable.Class (walk)
+import Control.Walkable.THExp (walkExpImpl, ExpHandler(ExpHandler))
 
 lcase :: [a -> b] -> a -> b
 lcase = undefined
 
-handleLCase expQ = expQ >>= descentLCase
-
-descentLCase :: Data d => d -> Q d
-descentLCase = gmapM handler
+lcaseHandler = ExpHandler handle
   where
-    handler :: Data d => d -> Q d
-    handler = recurse `extM` (\case
-                AppE lcaseV clauses | lcaseV == (VarE 'lcase) ->
-                  do
-                    var <- newName "var"
-                    clauses' <- case clauses of
-                                  ListE cl -> return cl
-                                  _ -> fail ("handleLCase: clauses must me list of lambdas: " ++ show (ppr clauses))
-                    clauses'' <-
-                      mapM (\v -> case v of
-                                    LamE [p] e -> liftM (\e' -> Match p (NormalB e') []) $ descentLCase e
-                                    _ -> fail ("handleLCase: only single-pattern lambda is allowed in clauses: " ++ show (ppr v)))
-                            clauses'
-                    return (LamE [VarP var] (CaseE (VarE var) clauses''))
-                e -> recurse e
-              )
-    -- recurse :: Data d => d -> Q d
-    recurse v = gmapM descentLCase v
+    handle (AppE lcaseV clauses) | lcaseV == (VarE 'lcase) =
+      do
+        var <- newName "var"
+        clauses' <- case clauses of
+                      ListE cl -> return cl
+                      _ -> fail ("handleLCase: clauses must me list of lambdas: " ++ show (ppr clauses))
+        clauses'' <-
+          mapM (\v -> case v of
+                        LamE [p] e -> liftM (\e' -> Match p (NormalB e') []) $ walk lcaseHandler e
+                        _ -> fail ("handleLCase: only single-pattern lambda is allowed in clauses: " ++ show (ppr v)))
+                clauses'
+        return (LamE [VarP var] (CaseE (VarE var) clauses''))
+    handle exp = walkExpImpl lcaseHandler exp
+
+handleLCase expQ = expQ >>= walk lcaseHandler
